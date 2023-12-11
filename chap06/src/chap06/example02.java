831fd02a5bef9f87cfbe567e404051accf6bad6a
shpark <shpark@172.16.9.202>
2023-12-11 19:30:01 +0900

    chap06

--- chap06/src/chap06/example02.java
+++ chap06/src/chap06/example02.java
@@ -4,7 +4,10 @@ public class example02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[] myArr = {10, 20, 30, 40, 50};
+		
+		for (int i = 0; i < 5; i++) {
+			System.out.println(i + " 번째 요소의 값 : " + myArr[i]);
+		}
 	}
-
 }
