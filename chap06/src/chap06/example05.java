a43a341f6aa573a682c0f95e7ecc88f70a198117
shpark <shpark@172.16.9.202>
2023-12-11 19:33:39 +0900

    chap06

--- chap06/src/chap06/example05.java
+++ chap06/src/chap06/example05.java
@@ -4,14 +4,7 @@ public class example05 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String s1 = "Java";
-		String s2 = "Java";
-		String s3 = new String("Java");
-		String s4 = new String("Java");
-		System.out.println(s1);
-		System.out.println(s2);
-		System.out.println(s3);
-		System.out.println(s4);
+
 	}
 
 }
